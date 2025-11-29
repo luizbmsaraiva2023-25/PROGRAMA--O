36181de43851ec6e50476f5241e496efdfbbb25f
luizbmsaraiva2023-25 <luizbmsaraiva2023@gmail.com>
2025-11-28 21:37:06 -0300

    mudanca

---
+++ CSS/CONVERSOR DE MOEDAS/scripts.js
@@ -0,0 +1,15 @@
+const convetButton = document.querySelector(".convert-button")
+
+function convertValues(){
+    const imputCurrencyValue = document.querySelector(".imput-currency").value
+
+    console.log(imputCurrencyValue)
+    
+}
+
+
+conertButton.addEventListener("click", convertValues)
+
+
+
+
